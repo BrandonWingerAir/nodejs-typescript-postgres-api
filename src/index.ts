e64d44751acf1da81387655bf5f296b3ec8b047d
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-11-12 03:05:51 -0500

    update routes to async and display errors

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,28 @@ import * as bodyParser from "body-parser"
 import { Request, Response } from "express"
 import { AppDataSource } from "./data-source"
 import { Routes } from "./routes"
-import { User } from "./entity/User"
 
 AppDataSource.initialize().then(async () => {
 
     // create express app
     const app = express()
-    app.use(bodyParser.json())
+    app.use(bodyParser.json());
 
     // register express routes from defined application routes
     Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next)
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result)
+        (app as any)[route.method](route.route, async (req: Request, res: Response, next: Function) => {
+            try {
+                const result = await (new (route.controller as any))[route.action](req, res, next);
+                res.json(result);
+            } catch(error) {
+                next(error);
             }
-        })
-    })
-
-    // setup express app here
-    // ...
+        });
+    });
 
     // start express server
-    app.listen(3000)
+    app.listen(3000);
 
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
+    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
 
-}).catch(error => console.log(error))
+}).catch(error => console.log(error));
