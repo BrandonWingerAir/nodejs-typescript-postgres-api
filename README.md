31fc1713a8c40d56a265beeb4894d880d620c66e
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-11-13 16:58:20 -0500

    test: prevent users without a first name

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ Steps to run this project:
 2. Setup database settings inside `data-source.ts` file
 3. Run `npm start` command
 
-## Note:
+## Study Notes:
 
 createConnection deprecated, updated to AppDataSource.
+
+### Test - should not create a user without a first name (updated response):
+response.body.errors[0]).toEqual({ 
+    type: 'field', 
+    msg: 'Invalid value', 
+    path: 'firstName', 
+    location: 'body' 
+});
--- __tests__/acceptance/users.test.ts
+++ __tests__/acceptance/users.test.ts
@@ -2,6 +2,7 @@ import { AppDataSource } from "./../../src/data-source";
 import * as request from 'supertest';
 import app from "../../src/app";
 import { port } from "../../src/config";
+import { param } from "express-validator";
 
 let connection, server;
 
@@ -25,14 +26,28 @@ afterEach(() => {
 // Tests
 it('initialize with no users', async() => {
     const response = await request(app).get('/users');
-
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual([]);
 });
 
 it('should create a user', async() => {
     const response = await request(app).post('/users').send(testUser);
-
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({ ...testUser, id: 1 });
-})
+});
+
+
+it('should not create a user without a first name', async() => {
+    const response = await request(app).post('/users').send({ lastName: 'User', age: 21 });
+
+    expect(response.statusCode).toBe(400);
+
+    expect(response.body.errors).not.toBeNull();
+    expect(response.body.errors.length).toBe(1);
+    expect(response.body.errors[0]).toEqual({ 
+        type: 'field', 
+        msg: 'Invalid value', 
+        path: 'firstName', 
+        location: 'body' 
+    });
+});
