270ebad8860443836ac7a50e1b0fd9a124d5f0bc
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-11-13 16:30:28 -0500

    setup Jest & SuperTest and initial app test

--- __tests__/acceptance/users.test.ts
+++ __tests__/acceptance/users.test.ts
@@ -1,3 +1,24 @@
-it('simple test', () => {
-    expect(1 + 1).toBe(2);
+import { AppDataSource } from "./../../src/data-source";
+import * as request from 'supertest';
+import app from "../../src/app";
+import { port } from "../../src/config";
+
+let connection, server;
+
+beforeEach(async() => {
+    connection = await AppDataSource.initialize();
+    await connection.synchronize();
+    server = app.listen(port);
+});
+
+afterEach(() => {
+    connection.close();
+    server.close();
+});
+
+it('initialize with no users', async() => {
+    const response = await request(app).get('/users');
+    
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual([]);
 });
