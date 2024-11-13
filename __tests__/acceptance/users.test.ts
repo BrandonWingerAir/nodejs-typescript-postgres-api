9f1a928dc0540e3199f6b2861cbd1ead4e182813
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-11-13 16:39:47 -0500

    test: create a user

--- __tests__/acceptance/users.test.ts
+++ __tests__/acceptance/users.test.ts
@@ -5,9 +5,15 @@ import { port } from "../../src/config";
 
 let connection, server;
 
+const testUser = {
+    firstName: 'Test',
+    lastName: 'User',
+    age: 30
+};
+
 beforeEach(async() => {
     connection = await AppDataSource.initialize();
-    await connection.synchronize();
+    await connection.synchronize(true);
     server = app.listen(port);
 });
 
@@ -16,9 +22,17 @@ afterEach(() => {
     server.close();
 });
 
+// Tests
 it('initialize with no users', async() => {
     const response = await request(app).get('/users');
-    
+
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual([]);
 });
+
+it('should create a user', async() => {
+    const response = await request(app).post('/users').send(testUser);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ ...testUser, id: 1 });
+})
